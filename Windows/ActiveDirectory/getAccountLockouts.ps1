be146500149759ae75af47b361ef4ac151b0a771
Joey S. Amalei <79823721+jamalei@users.noreply.github.com>
2022-02-19 08:56:30 +1000

    Added PowerShell scripts
    Added PowerShell scripts used for Active Directory administration

---
+++ Windows/ActiveDirectory/getAccountLockouts.ps1
@@ -0,0 +1,40 @@
+﻿# Program Title: getAccountLockouts.ps1
+# Last Updated By: 03/26/2021 - BCC - Joey S. Amalei
+# Purpose: Prints a list of domain account lockouts
+#
+# Notes:
+# -(01/20/2022) - Added input prompt to enter target server and admin user for query process
+#
+
+# Clear console screen
+Clear-Host
+
+Write-Host "============================="
+Write-Host "= GET ACCOUNT LOCKOUTS      ="
+Write-Host "= ------------------------- ="
+Write-Host "=   BLACK CONSTRUCTION CORP ="
+Write-Host "============================="
+Write-Host
+Write-Host
+
+# INPUT
+# -----
+# Get user input of domain user name to lookup
+$computerName = Read-Host "Enter target server computer name"
+$userName = Read-Host "Enter admin domain user name"
+Write-Host
+
+# PROCESS
+# -------
+# Perform domain query of users who were locked out:
+# Target Domain OU: "Black Construction"
+Get-WinEvent -ComputerName $computerName -Credential $userName -FilterHashtable @{logname=’security’; id=4740} | fl
+
+# OUTPUT
+# ------
+# Display to console screen that report has been printed
+Write-Host "------------------"
+Write-Host "Process completed."
+Write-Host "Good-bye!"
+Write-Host
+Read-Host "Press enter to continue"
